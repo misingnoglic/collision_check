e0f99fec7a6bc2573441b2aad7c16853d3214f01
arya <aboudaie@brandeis.edu>
2016-02-05 10:13:50 +0530

    Added more comments

--- collisions.py
+++ collisions.py
@@ -1,9 +1,9 @@
-from __future__ import division
+from __future__ import division # real division instead of integer division
 from random import SystemRandom
 import time
 from math import sqrt, ceil
 
-def check_collisions(N):
+def check_collisions(upper_bound):
     """
     Function that keeps guessing random numbers 1 to N incluside, until it hits a collision (or doesn't)
     """
@@ -11,28 +11,36 @@ def check_collisions(N):
     already_found = set([]) # Set of numbers the adversary already found
     iterations = 1
     start = time.time()
+    found = False
     try:
-        while True:
-            item = cryptogen.randrange(1,N)
-            if item in already_found: 
+        while not found: # Runs until a collision is found
+            item = cryptogen.randrange(1,upper_bound) # Uses the cryptographically secure PRNG to generate a random number
+            if item in already_found: # If it's in the set of things already found - print the statistics
+                found = True
                 print "duplicate found in %.2e tries in %f seconds" % (iterations, time.time()-start)
-                print "The upper bound on this probability is %.2f %%" % (coll(iterations,N)*100)
-                return
-            else:
+                print "The upper bound on this probability is %.2f %%" % (coll(iterations,upper_bound)*100)
+            else: # If it's a new number, add it to the set of numbers checked
                 already_found.add(item)
                 iterations+=1
-    except KeyboardInterrupt:
+    except KeyboardInterrupt: # If someone cancels the program midway - prints some statistics about the current progress
         total_time = time.time()-start
         print "Program cancelled - made %.2e attempts in %.4f seconds" % (iterations, total_time)
-        print "The upper bound on getting a duplicate is %.2f %%" % (coll(iterations,N)*100)
-        onepercent = ntimes(.01,N)
+        print "The upper bound on getting a duplicate is %.2f %%" % (coll(iterations,upper_bound)*100)
+        onepercent = ntimes(.01,upper_bound)
         rate = total_time/iterations
         print "To have 1%% probability of guessing you need at least %d tries, at this rate it would take %f seconds" % (onepercent, onepercent*rate)
-        raise
+        
             
             
 def coll(q,N):
+    """
+    Upper bound on the probability of a collision with q items chosen with an upper bound of N
+    """
     return float((q**2)/(2*N))
     
 def ntimes(P,N):
+    """
+    Similar to coll - but the equation is solved for q, not the probability. 
+    This is the lower bound of items needed to check for P probability of finding a collision from random numbers
+    """
     return ceil(sqrt(2*N*P))
